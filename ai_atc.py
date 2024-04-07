d4b2451027ec9a039d6fc260346f066cb8688eb4
Wei Wu <drweiwu@outlook.com>
2024-04-06 18:34:52 -0700

    added reponse in audio

--- ai_atc.py
+++ ai_atc.py
@@ -1,10 +1,21 @@
 from openai import OpenAI
+from pathlib import Path
 import json
 from dotenv import load_dotenv
+from pygame import mixer  # Load the popular external library
+#import azure.cognitiveservices.speech as speechsdk
 
 load_dotenv()
 client = OpenAI()
 
+def generate_voice(client, texts, voice="alloy"):
+    response = client.audio.speech.create(
+        model="tts-1-hd",
+        voice=voice,
+        input=texts
+    )
+    
+    return response
 
 ordering = []
 
@@ -99,7 +110,7 @@ atc_functions = [
             "type": "function",
             "function": {
                 "name": "get_airplane_priotity",
-                "description": "Get the piroitize the airplane given the airplace ID",
+                "description": "Get the piroitity of the airplane in the current ordering given the airplace ID",
                 "parameters": {
                     "type": "object",
                     "properties": {
@@ -166,25 +177,29 @@ def get_airplace_distance(airplane_id):
 
 def queue_airplane(airplane_id):
     """Queue airplane given the airplace ID"""
-    ordering.append(airplane_id)
+    ordering.append(airplane_id["airplane_id"])
+    print("\n----current ordering----")
     print(ordering)
+    print("------------------------\n")
 
-    return json.dumps({"airplane_id": airplane_id, "queue": ordering.index(airplane_id) + 1})
+    return json.dumps({"airplane_id": airplane_id, "queue": ordering.index(airplane_id["airplane_id"]) + 1})
 
 
 def prioritize_airplane(airplane_id):
     """Piroitize the airplane given the airplace ID"""
     if airplane_id in ordering:
-        ordering.remove(airplane_id)
-    ordering.insert(0, airplane_id)
+        ordering.remove(airplane_id["airplane_id"])
+    ordering.insert(0, airplane_id["airplane_id"])
+    print("\n----current ordering----")
     print(ordering)
+    print("------------------------\n")
 
     return json.dumps({"airplane_id": "N31469", "priority": "1"})
 
 def get_airplane_priotity(airplane_id):
     """Get the piroitize the airplane given the airplace ID"""
-    if airplane_id in ordering:
-        return json.dumps({"airplane_id": airplane_id, "priority": ordering.index(airplane_id) + 1})
+    if airplane_id["airplane_id"] in ordering:
+        return json.dumps({"airplane_id": airplane_id["airplane_id"], "priority": ordering.index(airplane_id["airplane_id"]) + 1})
     else:
         return json.dumps({"airplane_id": airplane_id, "priority": "unknown"})
 
@@ -201,8 +216,8 @@ def run_conversation(request="What's the weather like in San Francisco, Tokyo, a
     )
     response_message = response.choices[0].message
     tool_calls = response_message.tool_calls
-    print(response_message)
-    print(tool_calls)
+    #print(response_message)
+    #print(tool_calls)
     # Step 2: check if the model wanted to call a function
     if tool_calls:
         # Step 3: call the function
@@ -237,7 +252,46 @@ def run_conversation(request="What's the weather like in San Francisco, Tokyo, a
             model="gpt-3.5-turbo-0125",
             messages=messages,
         )  # get a new response from the model where it can see the function response
+
+        voice = "shimmer" #"nova" if i%2 ==0 else "alloy"
+        response = generate_voice(client, second_response.choices[0].message.content, voice=voice)
+        text = second_response.choices[0].message.content
+        speech_file_path = Path(__file__).parent / ("reply.mp3")
+        response.stream_to_file(speech_file_path)
+
+        mixer.init()
+        mixer.music.load(speech_file_path)
+        mixer.music.play()
+        """
+
+        # Creates an instance of a speech config with specified subscription key and service region.
+        speech_key = "6eaea78eb87848b2be4c99bcce78ead7"
+        service_region = "eastus"
+
+        speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+        # Note: the voice setting will not overwrite the voice element in input SSML.
+        speech_config.speech_synthesis_voice_name = "zh-CN-liaoning-XiaobeiNeural"
+
+        # use the default speaker as audio output.
+        speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+        file_config = speechsdk.audio.AudioOutputConfig(filename="outputaudio.wav")  
+        speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=file_config)  
+
+        result = speech_synthesizer.speak_text_async(text).get()
+        # Check result
+        if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+            print("Speech synthesized for text [{}]".format(text))
+        elif result.reason == speechsdk.ResultReason.Canceled:
+            cancellation_details = result.cancellation_details
+            print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+            if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                print("Error details: {}".format(cancellation_details.error_details))
+        """
+
         return second_response
 #print(run_conversation())
 print(run_conversation("N31469 request to land"))
+print(run_conversation("N31480 request to land"))
+print(run_conversation("What is the order of N31480?"))
 print(run_conversation("28000 request to land, and prioritize 28000"))
